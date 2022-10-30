7fae1ee57f36968a9ee20df78e2676e580d3cc34
Pain <mohammedbrham98@gmail.com>
2022-10-30 15:05:11 +0300

    Add converters files

---
+++ converters.py
@@ -0,0 +1,6 @@
+def lbs_to_ks(weight):
+    return weight * 0.45
+
+
+def ks_to_lbs(weight):
+    return weight / 0.45
---
+++ converters/app.py
@@ -0,0 +1,10 @@
+# First syntax to import a function from the a module
+import converters
+
+# Second syntax to import a function from a module
+# import specific function
+from converters import ks_to_lbs
+
+converters.ks_to_lbs(345)
+
+ks_to_lbs(34)
