c87722b36f9c31013ba8f1e63f4b8a0f438486fb
Pain <mohammedbrham98@gmail.com>
2022-10-30 15:41:58 +0300

    Random_module

---
+++ Random_values/app.py
@@ -0,0 +1,9 @@
+import random
+
+# for i in range(3):
+#     print(random.random())
+#     print(random.randint(a=10, b=20))
+
+members = ['John', 'Mary', 'Bob', 'Mosh']
+leader = random.choice(members)
+print(leader)
